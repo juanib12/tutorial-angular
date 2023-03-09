a39699fa6b30f6a7ac39f3bb87bfc87a2c8c7034
juanib12 <bjuan.ignacio11@gmail.com>
2023-03-09 14:43:53 -0300

    tutorial-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgOptimizedImage} from '@angular/common'
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
